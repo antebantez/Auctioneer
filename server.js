eb758e5f04f1afff8e418f6275315ae63817299e
Anton Olsson <antonolsson.imp@gmail.com>
2022-03-17 11:10:00 +0100

    Added some folders for each database

--- server.js
+++ server.js
@@ -3,13 +3,6 @@ const server = express();
 // ta emot json i request body
 server.use(express.json());
 
-// middleware (exempel på hur detta med "use" fungerar, som i server.use)
-server.use((request, response, next) => {
-  request.hello = "world";
-  response.header("X-goodbye", "cruel world");
-  next();
-});
-
 // lägg till session-hantering
 session = require("express-session");
 server.use(
@@ -27,49 +20,3 @@ server.listen(3000, () => {
 });
 
 // data
-const mongoose = require("mongoose");
-
-// mongoose models
-const MenuItem = mongoose.model(
-  "MenuItem",
-  new mongoose.Schema({
-    name: String,
-    price: Number,
-  })
-);
-
-const Menu = mongoose.model(
-  "Menu",
-  new mongoose.Schema({
-    name: String,
-    menuitems: [{ type: mongoose.Schema.Types.ObjectId, ref: "MenuItem" }],
-  })
-);
-
-const Customer = mongoose.model(
-  "Customer",
-  new mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    email: { type: String, unique: true, required: true },
-    password: String,
-  })
-);
-
-// rest api
-const RestCustomer = require("./rest-api/customer.js");
-const RestAuthentication = require("./rest-api/authentication.js");
-const RestMenu = require("./rest-api/menu.js");
-const RestMenuItems = require("./rest-api/menuitems.js");
-
-async function start() {
-  await mongoose.connect(
-    "mongodb+srv://auctioneer:HaNYxwm5bLEhisow@actioneer.teqmr.mongodb.net/auctioneerdb?retryWrites=true&w=majority"
-  );
-  // add REST api
-  RestCustomer(server, Customer);
-  RestAuthentication(server, Customer);
-  RestMenu(server, Menu);
-  RestMenuItems(server, MenuItem);
-}
-start();
