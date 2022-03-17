86cc51fd15a20c95b023aa1c013803298d0a6262
Anton Olsson <antonolsson.imp@gmail.com>
2022-03-17 09:48:54 +0100

    Changed from LF to CRLF

--- rest-api/customer.js
+++ rest-api/customer.js
@@ -1,11 +1,9 @@
 // Registration
 
-module.exports = function(server, Customer){
-
-    server.post('/data/customer', async (request, response) => {
-        let item = new Customer(request.body)
-        let result = await item.save()
-        response.json(result)
-    })
-
-}
+module.exports = function (server, Customer) {
+  server.post("/data/customer", async (request, response) => {
+    let item = new Customer(request.body);
+    let result = await item.save();
+    response.json(result);
+  });
+};
